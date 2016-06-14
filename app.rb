74808d369afbc5a420d2e8b92585cd9cea2bcdb6
a-mellor <alexander.mellor@gmail.com>
2016-06-14 16:42:15 +0100

    starting to use heroku

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+ENV["RACK_ENV"] ||= 'development'
 require 'sinatra/base'
 require_relative 'lib/viewing_links'
 
@@ -12,13 +13,13 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/links' do
-    Link.create(url: params[:URL], title: params[:Title])
+    Link.create(url: params[:url], title: params[:title])
     redirect '/links'
   end
 
   get '/links/new' do
     erb(:'links/new')
   end
-  # start the server if ruby file executed directly
+
   run! if app_file == $0
 end
--- lib/viewing_links.rb
+++ lib/viewing_links.rb
@@ -1,6 +1,7 @@
 require 'data_mapper'
 require 'dm-postgres-adapter'
 
+
 class Link
   include DataMapper::Resource
 
@@ -9,9 +10,10 @@ class Link
   property(:url, String)
 end
 
-
+p '==================='
+p ENV['DATABASE_URL']
 # Now let's set up a connection with a database
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_test")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 # Let's check that everything we wrote in our models was OK
 DataMapper.finalize
 # And let's build any new columns or tables we added
