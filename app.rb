14e14b2fb8e9f321a4f32db1e62f6176a4375e6f
a-mellor <alexander.mellor@gmail.com>
2016-06-16 12:30:54 +0100

    Ch 17- Adding user accounts. Featuer added

--- app.rb
+++ app.rb
@@ -4,6 +4,9 @@ require './models/link'
 require_relative 'data_mapper_setup'
 
 class Bookmarks < Sinatra::Base
+  #
+  enable :sessions
+  set :session_secret, 'super secret'
 
   get '/' do
     redirect '/links'
@@ -35,6 +38,22 @@ class Bookmarks < Sinatra::Base
     erb :'links/index'
   end
 
+  get '/users/new' do
+    erb :'users/new'
+  end
+
+  post '/users' do
+    user = User.create(email: params[:email],
+                password: params[:password])
+    session[:user_id] = user.id
+    redirect to('/links')
+  end
+
+  helpers do
+    def current_user
+      @current_user ||= User.get(session[:user_id])
+    end
+  end
 
   run! if app_file == $0
 end
